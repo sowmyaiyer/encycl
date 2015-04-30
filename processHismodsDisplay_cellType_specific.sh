f095935863ee693bb1f34a7c1ad32b401984ba85
Sowmya Iyer <sowmyai@bu.edu>
2015-04-30 10:54:13 -0400

    added code to split files.

--- processHismodsDisplay_cellType_specific.sh
+++ processHismodsDisplay_cellType_specific.sh
@@ -1,4 +1,15 @@
 #for inputFile in $HOME/nearline/decorate_dnase/dnase_master_V2/splits/*.chr*
+rm /home/si14w/nearline/decorate_dnase/dnase_master_V2/splits/multi-tissue.master.v2.*.*.bed.chr*.bed
+awk '{ print $0 >> "/home/si14w/nearline/decorate_dnase/dnase_master_V2/splits/multi-tissue.master.v2.distal.H3K27ac.bed."$1".bed"}' /home/si14w/nearline/decorate_dnase/dnase_master_V2/multi-tissue.master.v2.distal.H3K27ac.bed
+awk '{ print $0 >> "/home/si14w/nearline/decorate_dnase/dnase_master_V2/splits/multi-tissue.master.v2.distal.H3K4me1.bed."$1".bed"}' /home/si14w/nearline/decorate_dnase/dnase_master_V2/multi-tissue.master.v2.distal.H3K4me1.bed
+awk '{ print $0 >> "/home/si14w/nearline/decorate_dnase/dnase_master_V2/splits/multi-tissue.master.v2.distal.H3K9ac.bed."$1".bed"}' /home/si14w/nearline/decorate_dnase/dnase_master_V2/multi-tissue.master.v2.distal.H3K9ac.bed
+awk '{ print $0 >> "/home/si14w/nearline/decorate_dnase/dnase_master_V2/splits/multi-tissue.master.v2.distal.H3K4me3.bed."$1".bed"}' /home/si14w/nearline/decorate_dnase/dnase_master_V2/multi-tissue.master.v2.distal.H3K4me3.bed
+
+awk '{ print $0 >> "/home/si14w/nearline/decorate_dnase/dnase_master_V2/splits/multi-tissue.master.v2.proximal.H3K27ac.bed."$1".bed"}' /home/si14w/nearline/decorate_dnase/dnase_master_V2/multi-tissue.master.v2.proximal.H3K27ac.bed
+awk '{ print $0 >> "/home/si14w/nearline/decorate_dnase/dnase_master_V2/splits/multi-tissue.master.v2.proximal.H3K4me1.bed."$1".bed"}' /home/si14w/nearline/decorate_dnase/dnase_master_V2/multi-tissue.master.v2.proximal.H3K4me1.bed
+awk '{ print $0 >> "/home/si14w/nearline/decorate_dnase/dnase_master_V2/splits/multi-tissue.master.v2.proximal.H3K9ac.bed."$1".bed"}' /home/si14w/nearline/decorate_dnase/dnase_master_V2/multi-tissue.master.v2.proximal.H3K9ac.bed
+awk '{ print $0 >> "/home/si14w/nearline/decorate_dnase/dnase_master_V2/splits/multi-tissue.master.v2.proximal.H3K4me3.bed."$1".bed"}' /home/si14w/nearline/decorate_dnase/dnase_master_V2/multi-tissue.master.v2.proximal.H3K4me3.bed
+
 for hismod in {"H3K27ac","H3K4me1","H3K4me3","H3K9ac"}
 do
 	for inputFile in $HOME/nearline/decorate_dnase/dnase_master_V2/splits/multi-tissue.master.v2.distal.${hismod}.bed.chr*.bed
